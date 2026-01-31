240c5d9a570f55373934066953a35916886914ec
Yuval Shavit <dev@yuvalshavit.com>
2026-01-31 16:06:46 -0500

    add tiktoken BPE

--- simpllm/src/main.rs
+++ simpllm/src/main.rs
@@ -1,3 +1,15 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    let tokenizer = tiktoken_rs::r50k_base_singleton();
+    for line in stdin().lines() {
+        let line = line.expect("error reading line. invalid utf-8?");
+        let tok_indexes = tokenizer.encode_with_special_tokens(&line);
+
+        print!("{tok_indexes:?} => ");
+
+        let result_str = tokenizer.decode(tok_indexes);
+        let result_str = result_str.unwrap();
+        println!("{result_str}");
+    }
 }
