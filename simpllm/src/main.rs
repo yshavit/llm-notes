53d84cfef254519d704f153e256c325cc1872255
Yuval Shavit <dev@yuvalshavit.com>
2026-02-05 00:18:28 -0500

    nicer main loop

--- simpllm/src/main.rs
+++ simpllm/src/main.rs
@@ -1,5 +1,5 @@
 use simpllm::load::{ModelPath, load_model};
-use std::io::stdin;
+use std::io::{Write, stdin, stdout};
 use tiktoken_rs::Rank;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -11,20 +11,30 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     eprint!("Loading tokenizer...");
     let tokenizer = tiktoken_rs::r50k_base_singleton();
     eprintln!("Ready!");
-    eprint!("> ");
 
-    for line in stdin().lines() {
-        let line = line.expect("error reading line. invalid utf-8?");
+    let mut line = String::new();
+
+    loop {
+        eprint!("> ");
+        line.clear();
+        stdin().read_line(&mut line)?;
+        let line = line.trim_end_matches(['\r', '\n']);
+        if line == "exit" {
+            break;
+        }
+
+        print!("{line}");
+        let _ = stdout().flush();
+
         let mut tok_indexes = tokenizer.encode_with_special_tokens(&line);
 
         // infer 10 times, hard-coded for now
-        for _ in 0..10 {
+        for _ in 0..50 {
             let result = model.apply(&tok_indexes)?;
             let last_row = result.num_rows() - 1;
             let result_rank = result.with_row([last_row, 0], |logits| {
                 let mut all = Vec::from(logits);
                 all.sort_by(|a, b| b.partial_cmp(a).unwrap_or(std::cmp::Ordering::Equal));
-                eprintln!("logits: {:?} ... {:?}", &all[..5], &all[all.len() - 5..]);
 
                 let mut max_idx = 0;
                 let mut max_val = logits[0];
@@ -37,9 +47,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 max_idx
             });
             tok_indexes.push(result_rank as Rank);
-            let result_str = tokenizer.decode(tok_indexes.clone())?;
-            println!("inferred: {result_str}");
+
+            let result_str = tokenizer
+                .decode(vec![result_rank as Rank])
+                .unwrap_or_else(|_| "<??>".to_string());
+            print!("{result_str}");
+            let _ = stdout().flush();
         }
+        println!("...");
     }
 
     Ok(())
