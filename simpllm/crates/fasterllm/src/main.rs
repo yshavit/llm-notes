e96d879b9c406215d818893aad642279b91be120
Yuval Shavit <dev@yuvalshavit.com>
2026-02-09 01:37:43 -0500

    CUDA is working, but very slowly

--- simpllm/crates/fasterllm/src/main.rs
+++ simpllm/crates/fasterllm/src/main.rs
@@ -69,7 +69,17 @@ impl<const R: usize> Tensor<R> for CandleTensor<R> {
     }
 
     fn matmul(&self, other: &Self) -> Self {
-        let c_tensor = self.c_tensor.matmul(&other.c_tensor).expect("couldn't perform matmul");
+        let lhs = if self.c_tensor.is_contiguous() {
+            self.c_tensor.clone()
+        } else {
+            self.c_tensor.contiguous().expect("couldn't make lhs contiguous")
+        };
+        let rhs = if other.c_tensor.is_contiguous() {
+            other.c_tensor.clone()
+        } else {
+            other.c_tensor.contiguous().expect("couldn't make rhs contiguous")
+        };
+        let c_tensor = lhs.matmul(&rhs).expect("couldn't perform matmul");
         Self { c_tensor }
     }
 
@@ -110,8 +120,10 @@ impl<const R: usize> Tensor<R> for CandleTensor<R> {
         let result = f(&mut vec);
 
         // Write the modified values back
+        let mut row_shape = vec![1; R - 1];
+        row_shape.push(self.shape()[R - 1]);
         let row_tensor =
-            candle_core::Tensor::from_slice(&vec, flat.shape(), flat.device()).expect("couldn't create tensor");
+            candle_core::Tensor::from_slice(&vec, row_shape, flat.device()).expect("couldn't create tensor");
         let mut slice_params = vec![];
         for (i, &idx) in indices.iter().enumerate().take(R - 1) {
             slice_params.push(idx..idx + 1);
@@ -137,7 +149,11 @@ impl<const R: usize> Tensor<R> for CandleTensor<R> {
     }
 
     fn softmax(self) -> Self {
-        let c_tensor = candle_nn::ops::softmax_last_dim(&self.c_tensor).expect("couldn't apply softmax");
+        let cpu_tensor = self.c_tensor.to_device(&Device::Cpu).expect("couldn't move to CPU");
+        let softmax_tensor = candle_nn::ops::softmax_last_dim(&cpu_tensor).expect("couldn't apply softmax");
+        let c_tensor = softmax_tensor
+            .to_device(self.c_tensor.device())
+            .expect("couldn't move back to CUDA");
         Self { c_tensor }
     }
 
@@ -155,7 +171,9 @@ impl<const R: usize> Tensor<R> for CandleTensor<R> {
 
     fn set_row(&mut self, indices: [usize; R], values: &[f32]) {
         let shape = self.shape();
-        let row_tensor = candle_core::Tensor::from_slice(values, (values.len(),), self.c_tensor.device())
+        let mut row_shape = vec![1; R - 1];
+        row_shape.push(shape[R - 1]);
+        let row_tensor = candle_core::Tensor::from_slice(values, row_shape, self.c_tensor.device())
             .expect("couldn't create tensor from slice");
 
         let mut slice_params = vec![];
